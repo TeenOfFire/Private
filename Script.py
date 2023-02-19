550c612fe8a9bfa6e94b5a741feb013667f7c127
Teen Fire <102863642+TeenOfFire@users.noreply.github.com>
2023-02-19 13:49:23 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -13,9 +13,9 @@ def is_enabled(value, default):
 class script(object):
     START_TXT = """<i><b>🎃 Helo {}, I'm <a href=https://telegram.me/{}>{}</a></i></b> \n\n<i><b>🎗I Can Provide You Any Movies, Web-Series, Anime, K-Dramas, Animation, etc.,</i></b>"""
     HELP_TXT = """<b>🥁 </b><b><u>How To Download Any Movie, Series, Anime etc., For Free???</u></b> \n\n<b>🎗Group [01]: </b><b>https://t.me/+WzsvFY3qXa9kZGVl</b> \n\n<b>🎗Group</b> <b>[02]: </b><b>https://t.me/+EdJU1Hqk1N80ZWQ1</b> \n\n<b>🔆</b> <b>Join Any Of These Groups</b>👆"""
-    ABOUT_TXT = """<i><u>🧶 </u></i><i><u><b>Follow These Steps To Connect Me To Your Group</b>👇</u>
+    ABOUT_TXT = """<i><b>🥁 Follow These Steps To Connect Me To Your Group👇</b>
 
-1. Click on This [</i><a href="http://telegram.me/heroriderbot?startgroup=true"><i>Blue Text</i></a><i>]
+1. Click on "</i><i><b>Click Here To Add Me</b>" 
 2. Select Your Group
 3. Make Me Admin in Your Group</i>"""    
     
